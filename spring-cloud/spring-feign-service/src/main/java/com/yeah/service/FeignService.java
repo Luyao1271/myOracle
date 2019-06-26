cdee88b7fbeea9af83eaeed345e3e662020112ed
LUYY <1451169768@qq.com>
2019-06-26 18:12:06 +0800

    Jar update

--- spring-cloud/spring-feign-service/src/main/java/com/yeah/service/FeignService.java
+++ spring-cloud/spring-feign-service/src/main/java/com/yeah/service/FeignService.java
@@ -1,11 +1,10 @@
 package com.yeah.service;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import com.yeah.service.impl.FeignServiceImpl;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.yeah.service.impl.FeignServiceImpl;
 
 @FeignClient(value = "provider-user",fallback=FeignServiceImpl.class)
 public interface FeignService {
